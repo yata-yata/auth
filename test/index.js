8807dad916c025d2cedf6cd00007724ad9602366
Mike Davis <mike@michaeljacobdavis.com>
2014-05-12 19:53:18 -0700

    Refactoed auth out to strategies

--- test/index.js
+++ test/index.js
@@ -2,18 +2,18 @@
 
 var Lab = require('lab'),
 
-  // Declare internals
-  internals = {},
+    // Declare internals
+    internals = {},
 
-  // Test aliases
-  expect = Lab.expect,
-  before = Lab.before,
-  beforeEach = Lab.beforeEach,
-  after = Lab.after,
-  afterEach = Lab.afterEach,
-  describe = Lab.experiment,
-  it = Lab.test,
-  assert = Lab.assert;
+    // Test aliases
+    expect = Lab.expect,
+    before = Lab.before,
+    beforeEach = Lab.beforeEach,
+    after = Lab.after,
+    afterEach = Lab.afterEach,
+    describe = Lab.experiment,
+    it = Lab.test,
+    assert = Lab.assert;
 
 describe('auth', function () {
 });
